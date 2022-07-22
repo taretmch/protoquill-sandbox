885de7aa379e9f39cbd7393699ad602e15b5a82b
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2022-07-22 17:58:25 +0900

    Update sample

--- src/main/scala/BasicQueries.scala
+++ src/main/scala/BasicQueries.scala
@@ -23,13 +23,28 @@ object BasicQueries:
     query[Person].insertValue(lift(Person("tomoki", "mizogami", 24))).returning(_.firstName)
   }
 
+  def update1 = 
+    val updated = Person("友貴", "溝上", 24)
+    quote {
+      query[Person].filter(p => p.firstName == "tomoki" && p.lastName == "mizogami").updateValue(lift(updated))
+    }
+
+  def delete1 =
+    quote {
+      query[Person].filter(_.lastName == "mizogami").delete
+    }
+
   inline def runSelect1 = ctx.run(select1)
   inline def runInsert1 = ctx.run(insert1)
   inline def runInsert2 = ctx.run(insert2)
   inline def runInsert3 = ctx.run(insert3)
+  inline def runUpdate1 = ctx.run(update1)
+  inline def runDelete1 = ctx.run(delete1)
 
   @main def runBasicQueries =
     println(runSelect1)
     println(runInsert1)
     println(runInsert2)
     println(runInsert3)
+    println(runUpdate1)
+    println(runDelete1)
