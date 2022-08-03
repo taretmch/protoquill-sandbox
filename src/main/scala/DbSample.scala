50cb2e1233d20718a326b6980d0d0ef8f37c84a7
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2022-08-03 22:56:15 +0900

    Add samples for liftQuery

--- src/main/scala/DbSample.scala
+++ src/main/scala/DbSample.scala
@@ -14,6 +14,42 @@ object DbSample:
     println("The result set is:")
     println(res)
 
+  @main def liftQuerySample =
+    val ctx = new MysqlJdbcContext(SnakeCase, "sample.context")
+    import ctx._
+
+    case class TestTable(id: Long, name: String)
+
+    val res = ctx.run(quote {
+      query[TestTable].filter(v => liftQuery(List(1L, 2L)).contains(v.id))
+    })
+    val res2 = ctx.run(quote {
+      query[TestTable].filter(v => liftQuery(List("hoge", "a")).contains(v.name))
+    })
+    println("The result set is:")
+    println(res)
+    println(res2)
+
+  @main def liftQuerySample2 =
+    val ctx = new MysqlJdbcContext(SnakeCase, "sample.context")
+    import ctx._
+
+    case class TestTable(id: TestTable.Id, name: String)
+    object TestTable:
+      opaque type Id = Long
+      object Id:
+        def apply(id: Long): Id = id
+        given MappedEncoding[Id, Long](id => id)
+        given MappedEncoding[Long, Id](id => id)
+
+    import TestTable.Id
+
+    val res = ctx.run(quote {
+      query[TestTable].filter(v => liftQuery(List(Id(1L), Id(2L))).contains(v.id))
+    })
+    println("The result set is:")
+    println(res)
+
   @main def insertTestTable =
     val ctx = new MysqlJdbcContext(SnakeCase, "sample.context")
     import ctx._
