b94839d7cfa454c089bd0a1bd3cf428b88be82ec
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2022-12-19 19:22:29 +0900

    Fix variable name

--- src/main/scala/RepositorySample.scala
+++ src/main/scala/RepositorySample.scala
@@ -3,33 +3,33 @@ import repository.{ Database, PersonRepository }
 
 object RepositorySample:
 
-  val db         = new Database
-  val repository = new PersonRepository(using db)
+  val db               = new Database
+  val personRepository = new PersonRepository(using db)
 
   @main def getPerson: Unit =
-    val persons = repository.findAll
+    val persons = personRepository.findAll
     persons.foreach(println _)
 
   @main def insertPerson: Unit =
     val data = Person(Person.FirstName("tomoki"), Person.LastName("mizogami"), Person.Age(25), Person.Gender.MALE)
-    val insertedId = repository.add(data)
+    val insertedId = personRepository.add(data)
     println("id of inserted value is " + insertedId)
 
   @main def updatePerson: Unit =
-    val persons = repository.findAll
+    val persons = personRepository.findAll
     val maxId   = Person.Id(persons.map(_.id.toLong).max)
     for
-      person <- repository.get(maxId)
+      person <- personRepository.get(maxId)
     yield
       val data = person.copy(age = person.age.increment)
-      repository.update(data)
+      personRepository.update(data)
       println("before: " + person)
       println("after:  " + data)
 
   @main def deletePerson: Unit =
-    val persons = repository.findAll
+    val persons = personRepository.findAll
     val maxId   = Person.Id(persons.map(_.id.toLong).max)
     for
-      deleted <- repository.delete(maxId)
+      deleted <- personRepository.delete(maxId)
     yield
       println("deleted person: " + deleted)
